09e76bd9b13ec2698b470568a55fcd42752c49a4
SupunWellappili <supunchandana1814@gmail.com>
2024-08-04 13:47:07 +0530

    file, path with import

--- index.js
+++ index.js
@@ -5,7 +5,13 @@ console.log(yName);
 console.log("///////////////");
 
 //os package
-const os = require('os');
+//const os = require('os');
+import os from 'os';
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(import.meta.url);
+
+
 console.log(os.platform());
 console.log(os.version());
 console.log(__dirname);
@@ -14,7 +20,9 @@ console.log(__filename);
 console.log("///////////////");
 
 //path package
-const path =require('path');
+//const path =require('path');
+import path from 'path';
+
 console.log(path.basename(__filename));
 console.log(path.basename(__dirname));
 console.log(path.extname(__filename));
