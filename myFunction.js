2669cc25a104c7e748d98ec9b5bcec7fd7306b17
SupunWellappili <supunchandana1814@gmail.com>
2024-08-05 00:48:25 +0530

    Import | Import default | Export | { }

---
+++ myFunction.js
@@ -0,0 +1,16 @@
+const math = (number01, number02, number03, number04) => {
+  const a = number01 + number02 * 2;
+  if (number03) {
+    a += number03 - 5;
+  } 
+   if (number04) {
+    a *= number04;
+  }
+  return   a;
+};
+
+
+console.log("myFunction -: "+math(2,3));
+
+//export {math}; //normal/object 
+export default math;
---
+++ test.js
@@ -0,0 +1,5 @@
+//import {math} from "./myFunction.js"
+import math from "./myFunction.js";
+
+console.log("test -: "+math(2,3));
+
