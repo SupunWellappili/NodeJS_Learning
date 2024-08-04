aba416fd8e2147556afa715629ff1e1fa10b2d85
SupunWellappili <supunchandana1814@gmail.com>
2024-08-04 12:24:58 +0530

    OS package intro

--- index.js
+++ index.js
@@ -2,3 +2,10 @@
 const yName = "Supun";
 console.log(yName);
 
+console.log("///////////////");
+
+const os = require('os');
+console.log(os.platform());
+console.log(os.version());
+console.log(__dirname);
+console.log(__filename);
