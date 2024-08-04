c1c3855a9dd7314d225c6aa5627ef13e163357c3
SupunWellappili <supunchandana1814@gmail.com>
2024-08-05 00:58:34 +0530

    Import Rename AS

--- test.js
+++ test.js
@@ -1,5 +1,9 @@
-//import {math} from "./myFunction.js"
-import math from "./myFunction.js";
+//import {math} from "./myFunction.js";
+//import {math as name} from "./myFunction.js";
 
-console.log("test -: "+math(2,3));
+//import math from "./myFunction.js";
+//console.log("test -: "+math(2,3));
+
+import name from  "./myFunction.js";
+console.log("testChange -: "+name(5,6));
 
