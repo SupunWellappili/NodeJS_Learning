892bcded3dbb8af7a3f6d9b6df59e2de03ff7f5a
SupunWellappili <supunchandana1814@gmail.com>
2024-08-04 12:34:38 +0530

    PATH package intro

--- index.js
+++ index.js
@@ -4,8 +4,19 @@ console.log(yName);
 
 console.log("///////////////");
 
+//os package
 const os = require('os');
 console.log(os.platform());
 console.log(os.version());
 console.log(__dirname);
 console.log(__filename);
+
+console.log("///////////////");
+
+//path package
+const path =require('path');
+console.log(path.basename(__filename));
+console.log(path.basename(__dirname));
+console.log(path.extname(__filename));
+
+
