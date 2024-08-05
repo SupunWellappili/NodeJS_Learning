9619d78e67357d8aafabd306d3616f921a8d2818
SupunWellappili <supunchandana1814@gmail.com>
2024-08-05 08:50:01 +0530

    node:os package added

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ console.log("///////////////");
 
 //os package
 //const os = require('os');
-import os from 'os';
+import os from 'node:os';
 import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(import.meta.url);
@@ -21,7 +21,7 @@ console.log("///////////////");
 
 //path package
 //const path =require('path');
-import path from 'path';
+import path from 'node:path';
 
 console.log(path.basename(__filename));
 console.log(path.basename(__dirname));
